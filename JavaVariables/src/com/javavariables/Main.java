1a0bd109b5808417810de25645aa025f2a691632
Pranav <gupta2004.pranav@gmail.com>
2021-06-13 22:16:25 +0530

    variable and javaliterals

---
+++ JavaVariables/src/com/javavariables/Main.java
@@ -0,0 +1,20 @@
+package com.javavariables;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // variables
+        float arr1 = 9;
+        float arr2 = 5;
+        float arr3 = 5 ;
+        System.out.println(arr1+arr2+arr3);
+        // primitive data types:-
+        /*
+        1. integral(int) :- byte, short, int, long
+        2. floats(decimal) :- float, double
+        3. char('')
+        4. boolean(true/false)
+        * */
+    }
+}
